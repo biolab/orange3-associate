20f0989fe102e6ce8f8e6d8cbd1ad41e289e2df1
Jernej Urankar <jerneju@gmail.com>
2018-01-17 14:23:42 +0100

    [FIX] Frequent Itemsets: self.output defined in init

--- orangecontrib/associate/widgets/owitemsets.py
+++ orangecontrib/associate/widgets/owitemsets.py
@@ -56,6 +56,7 @@ class OWItemsets(widget.OWWidget):
 
     def __init__(self):
         self.data = None
+        self.output = None
         self._is_running = False
         self.isRegexMatch = lambda x: True
         self.tree = QTreeWidget(self.mainArea,
