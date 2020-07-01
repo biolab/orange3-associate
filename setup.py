ebfaf64297010458e9bf0bfabffdba2924103f9a
Rok Gomiscek <rok.gomiscek@gmail.com>
2020-07-01 12:54:36 +0200

    Release 1.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from distutils.extension import Extension
 from os import path
 
-VERSION = '1.1.5'
+VERSION = '1.1.6'
 
 ENTRY_POINTS = {
     'orange3.addon': (
