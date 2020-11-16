95fed503f2d1f0eea5bcc2d67a7896e56648bd02
Ajda Pretnar <ajdapretnar@gmail.com>
2020-11-16 10:54:28 +0100

    OWAssociate: Test

---
+++ orangecontrib/associate/widgets/tests/test_owassociate.py
@@ -0,0 +1,15 @@
+from Orange.data import Table
+from Orange.widgets.tests.base import WidgetTest
+from orangecontrib.associate.widgets.owassociate import OWAssociate
+
+
+class TestOWAssociate(WidgetTest):
+    def setUp(self):
+        self.widget = self.create_widget(OWAssociate)
+
+    def test_rules_output(self):
+        market = Table("data/market-basket")
+        self.send_signal(self.widget.Inputs.data, market)
+        self.widget.controls.autoFind.click()
+        output = self.get_output(self.widget.Outputs.rules)
+        self.assertTrue(output)
