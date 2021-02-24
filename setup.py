126e029f7358950607846c34881d3a5f15567319
jakakokosar <jaka.kokosar@gmail.com>
2021-02-24 15:27:41 +0100

    Release 1.1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from distutils.extension import Extension
 from os import path
 
-VERSION = '1.1.7'
+VERSION = '1.1.8'
 
 ENTRY_POINTS = {
     'orange3.addon': (
