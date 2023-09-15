80b4983aa67929d0e993979f477180a76c1579dd
PrimozGodec <p.godec9@gmail.com>
2023-09-15 10:25:05 +0200

    Release version 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from distutils.extension import Extension
 from os import path
 
-VERSION = '1.2.0'
+VERSION = '1.2.1'
 
 ENTRY_POINTS = {
     'orange3.addon': (
